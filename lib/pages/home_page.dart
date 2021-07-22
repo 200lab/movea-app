d236efb9df5dfe0ae32f6a355eb12c8227ca1cea
Ngọc Quang <1812832@dlu.edu.vn>
2021-07-22 17:54:45 +0700

    color init

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:movea_app/values/app_color.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
---
+++ lib/values/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final Color lightBlue = Color(0xff48cae7); // opacity 0.1
+  static final Color blueMain = Color(0xff3D58F8);
+  static final Color blueIllustration = Color(0xff2C4BA1);
+  static final Color darkBlueIllustration = Color(0xff1E3577);
+  static final Color white = Color(0xffFFFFFF);
+  static final Color grey = Color(0xffABADBD);
+  static final Color darkGrey = Color(0xff42476A);
+  static final Color darkBackground = Color(0xff151D3B);
+  static final Color darkerBackground = Color(0xff0B0F2F);
+  static final Color veryDark = Color(0xff051138);
+}
