1a7f0482470ff46ff3a226f8599be3c1d9ef5c50
Ngọc Quang <1812832@dlu.edu.vn>
2021-07-22 19:19:32 +0700

    custom button done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,11 +12,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final maxHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    final maxWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+
+
     return Scaffold(
-        body: Container(
-      color: AppColor.blueMain,
-      child: Center(
-        child: CustomButton.btnDate()
+      body: Container(
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        color: AppColor.blueMain,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            //   CustomButton(maxWidth/5,maxHeight/8,AppColor.white,),
+            //   CustomButton(maxHeight/8,maxWidth/5)
+
+            CustomButton(width: maxWidth/5, height: maxHeight / 8,
+            clr: AppColor.white, child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('SAT'),
+                    Text('20')
+                  ],
+                ))
+        ],
       ),
     ));
   }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:movea_app/values/app_color.dart';
 
-class CustomButton {
-  static Widget btnDate() {
+// class CustomButton {
+//   static Widget btnDate(double width, double height, Color clr, Widget child) {
+//     return Container(
+//         height: height,
+//         width: width,
+//         decoration:
+//             BoxDecoration(color: clr, borderRadius: BorderRadius.circular(14)),
+//         child: child);
+//   }
+// }
+
+class CustomButton extends StatelessWidget {
+  final width;
+  final height;
+  final clr;
+  final child;
+
+  const CustomButton(
+      {Key? key,
+      @required this.width,
+      @required this.height,
+      @required this.clr,
+      @required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      height: 80,
-      width: 60,
-      decoration: BoxDecoration(
-        color: AppColor.white,
-        borderRadius: BorderRadius.circular(14)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('SAT'),
-          Text('21')
-        ],
-      ),
-    );
+        height: height,
+        width: width,
+        decoration:
+            BoxDecoration(color: clr, borderRadius: BorderRadius.circular(14)),
+        child: child);
   }
 }
